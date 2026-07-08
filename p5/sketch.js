aa1a1954b7450983f13a6f6fd9c3facef4809bbb
Nicoleluu <67698387+Nicoleluu@users.noreply.github.com>
2026-07-08 17:03:12 -0400

    Update sketch.js

--- p5/sketch.js
+++ p5/sketch.js
@@ -1,1 +1,47 @@
+let chair;
+let reveal;
 
+function preload() {
+  chair = loadImage("chair.png");
+}
+
+function setup() {
+  let canvas = createCanvas(900,600);
+canvas.parent("chair-sketch");
+  
+  reveal = createGraphics(width,height);
+  reveal.background(247,244,238);
+}
+
+function draw() {
+
+  background(247,244,238);
+
+  let scale = 0.75;
+
+  let w = chair.width * scale;
+  let h = chair.height * scale;
+
+  let x = (width-w)/2;
+  let y = (height-h)/2;
+
+  image(chair,x,y,w,h);
+
+  if(mouseIsPressed){
+
+    reveal.erase();
+
+    reveal.circle(mouseX,mouseY,70);
+
+    reveal.noErase();
+
+  }
+
+  image(reveal,0,0);
+
+  noFill();
+  stroke(90);
+  strokeWeight(1);
+  circle(mouseX,mouseY,70);
+
+}
